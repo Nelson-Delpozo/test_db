47d505e016c7c8304736489ec1a40ff0730d4548
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-17 16:17:02 -0500

    order by

---
+++ limit_exercises.sql
@@ -0,0 +1,7 @@
+USE employees;
+
+SHOW TABLES;
+
+SELECT DISTINCT last_name FROM employees  ORDER BY last_name DESC LIMIT 10;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 46;
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,8 +2,6 @@ USE employees;
 
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY last_name, first_name;
 
-
-
 SELECT * FROM employees;
 #
 SELECT * FROM employees WHERE last_name LIKE '%q%';
@@ -18,7 +16,15 @@ SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%' ORDER B
 
 SELECT * FROM employees WHERE last_name LIKE 'e%e' ORDER BY emp_no DESC;
 
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name LIKE 'M%'
+LIMIT 10;
 
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name LIKE 'M%'
+LIMIT 25 OFFSET 50;
 
 
 
