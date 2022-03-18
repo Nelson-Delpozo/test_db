47ae8e97fd3507c1eafbfb05753305ce6a34459f
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-18 12:01:41 -0500

    finished group by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -13,5 +13,12 @@ LIMIT 10;
 
 SELECT DISTINCT title FROM titles;
 
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name, first_name;
+
+SELECT last_name, count(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT gender, count(gender) FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' GROUP BY gender;
 
 
