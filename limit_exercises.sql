5875b29b749589a8cbc0a98d2994649c0c85f930
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-18 10:08:37 -0500

    functions

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,3 +7,6 @@ SELECT DISTINCT last_name FROM employees  ORDER BY last_name DESC LIMIT 10;
 SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 46;
 
 DESCRIBE salaries;
+
+SELECT NOW();
+SELECT CURTIME();
