d36bbae077c6743fc251fbe1d40430400174416d
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-18 12:17:57 -0500

    alias exercises

---
+++ alias_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+LIMIT 25;
+
+SELECT hire_date, COUNT(*)
+FROM employees
+GROUP BY hire_date
+ORDER BY COUNT(*) DESC
+LIMIT 10;
+
+SELECT hire_date, COUNT(*) as number_hired
+FROM employees
+GROUP BY hire_date
+ORDER BY number_hired DESC
+LIMIT 10;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees AS emp
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 25;
+
