aa5a1bbdfe6d36aa49e808e8bae5eb6455f31a23
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-18 11:02:34 -0500

    functions done

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,7 +8,7 @@ SELECT * FROM employees WHERE last_name LIKE '%q%';
 #
 SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%' ORDER BY emp_no;
 
-SELECT * FROM employees WHERE last_name LIKE 'e%e' ORDER BY emp_no;
+SELECT CONCAT (first_name, " ", last_name) FROM employees WHERE last_name LIKE 'e%e' ORDER BY emp_no;
 #
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
@@ -25,6 +25,10 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE first_name LIKE 'M%'
 LIMIT 25 OFFSET 50;
+
+SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND year(hire_date) BETWEEN 1990 AND 1999 ORDER BY birth_date ASC , hire_date DESC;
+
+SELECT FLOOR(datediff(CURDATE(), hire_date)/365) FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND year(hire_date) BETWEEN 1990 AND 1999;
 
 
 
