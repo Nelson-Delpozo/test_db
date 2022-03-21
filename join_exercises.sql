b33d47c0f7e6cccd60513ee98d8053e62d0eb3d9
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-21 09:58:56 -0500

    joins

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,28 +1,17 @@
 USE join_test_db;
 
 SELECT users.name as user_name, roles.name as role_name
-FROM users
-         JOIN roles ON users.role_id = roles.id;
+FROM users JOIN roles ON users.role_id = roles.id;
 
 SELECT users.name AS user_name, roles.name AS role_name
-FROM users
-         LEFT JOIN roles ON users.role_id = roles.id;
+FROM users LEFT JOIN roles ON users.role_id = roles.id;
 
 SELECT users.name AS user_name, roles.name AS role_name
-FROM users
-         RIGHT JOIN roles ON users.role_id = roles.id;
+FROM users RIGHT JOIN roles ON users.role_id = roles.id;
 
 SELECT users.name as user_name, roles.name as role_name
-FROM roles
-         LEFT JOIN users ON users.role_id = roles.id;
+FROM roles LEFT JOIN users ON users.role_id = roles.id;
 
-# SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-# FROM employees as e
-#          JOIN dept_emp as de
-#               ON de.emp_no = e.emp_no
-#          JOIN departments as d
-#               ON d.dept_no = de.dept_no
-# WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
 
 
