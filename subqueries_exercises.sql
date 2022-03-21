35ab9fe6cdad31f6780ca67865016c5cc7441255
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-21 12:28:22 -0500

    all but the bonus on sub-queries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,2 +1,11 @@
 USE employees;
 
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS name, employees.hire_date AS hire_date
+FROM employees
+WHERE hire_date = (SELECT hire_date FROM employees WHERE employees.emp_no = 101010);
+
+SELECT titles.title AS title, CONCAT(employees.first_name, ' ', employees.last_name) AS name
+FROM titles JOIN employees ON titles.emp_no = employees.emp_no
+WHERE employees.first_name = 'Aamod';
+
+
