48d3686c8d52fd10de30513867c7dd220573417b
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-21 12:10:36 -0500

    join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -15,20 +15,43 @@
 USE employees;
 
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-FROM employees as e JOIN dept_emp as de
-    ON de.emp_no = e.emp_no
+FROM employees as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
          JOIN departments as d
               ON d.dept_no = de.dept_no;
 
 SELECT dept_name
 FROM departments;
 
-SELECT emp_no FROM dept_manager;
+SELECT emp_no
+FROM dept_manager;
 
 SELECT departments.dept_name AS department, CONCAT(employees.first_name, ' ', employees.last_name) AS manager
-FROM departments JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
-JOIN employees on dept_manager.emp_no = employees.emp_no
+FROM departments
+         JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+         JOIN employees on dept_manager.emp_no = employees.emp_no
 WHERE to_date = '9999-01-01';
+
+SELECT departments.dept_name AS department, CONCAT(employees.first_name, ' ', employees.last_name) AS manager
+FROM departments
+         JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+         JOIN employees on dept_manager.emp_no = employees.emp_no
+WHERE to_date = '9999-01-01'
+  AND gender = 'F';
+
+SELECT titles.title AS title, COUNT(all title) AS count
+FROM titles
+JOIN employees ON titles.emp_no = employees.emp_no
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+WHERE dept_no = 'd009'
+AND titles.to_date = '9999-01-01'
+GROUP BY titles.title;
+
+
+
+
+
 
 
 
